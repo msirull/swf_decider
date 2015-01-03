eb5f144383c093ab4274ddb2f64e7105376a7385
msirull <martin@sirull.com>
2015-01-03 01:46:29 -0800

    Initial commit

---
+++ decider.py
@@ -0,0 +1,31 @@
+# hello_decider.py
+import boto.swf.layer2 as swf
+
+DOMAIN = 'demo'
+ACTIVITY = 'HelloWorld'
+VERSION = '1.0'
+TASKLIST = 'default'
+
+class HelloDecider(swf.Decider):
+
+    domain = DOMAIN
+    task_list = TASKLIST
+    version = VERSION
+
+    def run(self):
+        while True:
+            history = self.poll()
+            if 'events' in history:
+                # Find workflow events not related to decision scheduling.
+                workflow_events = [e for e in history['events']
+                    if not e['eventType'].startswith('Decision')]
+                last_event = workflow_events[-1]
+
+                decisions = swf.Layer1Decisions()
+                if last_event['eventType'] == 'WorkflowExecutionStarted':
+                    decisions.schedule_activity_task('saying_hi', ACTIVITY, VERSION, task_list=TASKLIST)
+                elif last_event['eventType'] == 'ActivityTaskCompleted':
+                    decisions.complete_workflow_execution()
+                self.complete(decisions=decisions)
+            if history is False:
+                history = "No decisions to make"
